027a3601e78bc308d5ac38240f9cf33908244809
Shawn Belton <shawn.belton@redkiteweb.co.uk>
2016-10-28 23:58:14 +0100

    Cab data object tests added.

--- dcc-webapp/src/main/java/uk/co/redkiteweb/dccweb/webapp/data/CabFunctionComparator.java
+++ dcc-webapp/src/main/java/uk/co/redkiteweb/dccweb/webapp/data/CabFunctionComparator.java
@@ -9,6 +9,62 @@ public class CabFunctionComparator implements Comparator<CabFunction> {
 
     @Override
     public int compare(final CabFunction cabFunctionOne, final CabFunction cabFunctionTwo) {
-        return cabFunctionOne.getNumber().compareTo(cabFunctionTwo.getNumber());
+        int compare;
+        if (cabFunctionOne==null) {
+            compare = compareFunctionOneNull(cabFunctionTwo);
+        } else {
+            compare = compareFunctionOneNotNull(cabFunctionOne, cabFunctionTwo);
+        }
+        return compare;
+    }
+
+    private int compareFunctionOneNotNull(final CabFunction cabFunctionOne,final CabFunction cabFunctionTwo) {
+        int compare;
+        if (cabFunctionTwo==null) {
+            compare = 1;
+        } else {
+            compare = compareNeitherNull(cabFunctionOne.getNumber(), cabFunctionTwo.getNumber());
+        }
+        return compare;
+    }
+
+    private int compareNeitherNull(final Integer numberOne,final Integer numberTwo) {
+        int compare;
+        if (numberOne==null) {
+            compare = compareNumberOneNull(numberTwo);
+        } else {
+            compare = compareNumberOneNotNull(numberOne, numberTwo);
+        }
+        return compare;
+    }
+
+    private int compareNumberOneNotNull(final Integer numberOne,final Integer numberTwo) {
+        int compare;
+        if (numberTwo==null) {
+            compare = 1;
+        } else {
+            compare = numberOne.compareTo(numberTwo);
+        }
+        return compare;
+    }
+
+    private int compareNumberOneNull(final Integer numberTwo) {
+        int compare;
+        if (numberTwo==null) {
+            compare = 0;
+        } else {
+            compare = -1;
+        }
+        return compare;
+    }
+
+    private int compareFunctionOneNull(final CabFunction cabFunctionTwo) {
+        int compare;
+        if (cabFunctionTwo==null) {
+            compare = 0;
+        } else {
+            compare = -1;
+        }
+        return compare;
     }
 }
---
+++ dcc-webapp/src/test/java/uk/co/redkiteweb/dccweb/webapp/data/CabFunctionTest.java
@@ -0,0 +1,40 @@
+package uk.co.redkiteweb.dccweb.webapp.data;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by shawn on 28/10/16.
+ */
+@RunWith(JUnit4.class)
+public class CabFunctionTest {
+
+    private CabFunction cabFunction;
+
+    @Before
+    public void setup() {
+        cabFunction = new CabFunction();
+        cabFunction.setName("Sound");
+        cabFunction.setNumber(1);
+        cabFunction.setState(Boolean.FALSE);
+    }
+
+    @Test
+    public void testName() {
+        assertEquals("Sound", cabFunction.getName());
+    }
+
+    @Test
+    public void testNumber() {
+        assertEquals(new Integer(1), cabFunction.getNumber());
+    }
+
+    @Test
+    public void testState() {
+        assertEquals(Boolean.FALSE, cabFunction.getState());
+    }
+}
