39cc9e61005020f449c8dcae98b20d2057352532
Shawn Belton <shawn.belton@redkiteweb.co.uk>
2016-11-11 21:54:20 +0000

    Update to add a halt button to set the speed to zero.

--- demo-interface/src/main/java/uk/co/redkiteweb/dccweb/demo/messages/DemoChangeSpeedMessage.java
+++ demo-interface/src/main/java/uk/co/redkiteweb/dccweb/demo/messages/DemoChangeSpeedMessage.java
@@ -1,6 +1,9 @@
 package uk.co.redkiteweb.dccweb.demo.messages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
+import uk.co.redkiteweb.dccweb.dccinterface.messages.ChangeSpeedMessage;
 import uk.co.redkiteweb.dccweb.dccinterface.messages.Message;
 import uk.co.redkiteweb.dccweb.dccinterface.messages.MessageResponse;
 
@@ -10,8 +13,12 @@ import uk.co.redkiteweb.dccweb.dccinterface.messages.MessageResponse;
 @Component("DemoChangeSpeedMessage")
 public class DemoChangeSpeedMessage extends AbstractDemoMessage implements DemoMessage {
 
+    private static final Logger LOGGER = LogManager.getLogger(DemoChangeSpeedMessage.class);
+
     @Override
-    public MessageResponse process(Message message) {
+    public MessageResponse process(final Message message) {
+        final ChangeSpeedMessage changeSpeedMessage = (ChangeSpeedMessage)message;
+        LOGGER.info(String.format("Speed changed to %s %d", changeSpeedMessage.getDirection(), changeSpeedMessage.getSpeed()));
         return getMessageResponse();
     }
 }
