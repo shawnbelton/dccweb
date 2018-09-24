f910db72358383e8215668f95b96be1d0ce22972
Shawn Belton <shawn.belton@redkiteweb.co.uk>
2018-09-24 21:15:51 +0100

    hashCode and equals created for CVPK.

--- dcc-interface/src/main/java/uk/co/redkiteweb/dccweb/data/model/CVPK.java
+++ dcc-interface/src/main/java/uk/co/redkiteweb/dccweb/data/model/CVPK.java
@@ -1,6 +1,7 @@
 package uk.co.redkiteweb.dccweb.data.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * Created by shawn on 08/08/16.
@@ -33,5 +34,19 @@ public class CVPK implements Serializable {
 
     public void setCvNumber(Integer cvNumber) {
         this.cvNumber = cvNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(decoderId, cvNumber);
+    }
+
+    @Override
+    public boolean equals(final Object object) {
+        boolean isEqual = false;
+        if (object instanceof CVPK) {
+            isEqual = this.hashCode() == object.hashCode();
+        }
+        return isEqual;
     }
 }
