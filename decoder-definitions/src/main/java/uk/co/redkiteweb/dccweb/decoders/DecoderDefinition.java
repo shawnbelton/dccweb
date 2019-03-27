0d26b7649dcba78647da5e3f1f32dfee62d76366
Shawn Belton <shawn.belton@redkiteweb.co.uk>
2019-03-27 22:04:04 +0000

    Build CV Definitions from local file only

--- decoder-definitions/src/main/java/uk/co/redkiteweb/dccweb/decoders/DecoderDefinition.java
+++ decoder-definitions/src/main/java/uk/co/redkiteweb/dccweb/decoders/DecoderDefinition.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import uk.co.redkiteweb.dccweb.decoders.model.CVDefinition;
+import uk.co.redkiteweb.dccweb.decoders.model.CVValue;
+import uk.co.redkiteweb.dccweb.decoders.model.CVValueOption;
 
 import javax.xml.XMLConstants;
 import javax.xml.namespace.QName;
@@ -17,6 +21,8 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.*;
 import java.io.*;
+import java.util.Collection;
+import java.util.TreeSet;
 
 /**
  * Created by shawn on 16/09/16.
@@ -29,7 +35,7 @@ public class DecoderDefinition {
 
     private Document decoderDefDocument;
     private String definitionsPath;
-
+    private Collection<CVDefinition> cvDefinitions;
 
     @Value("${dccweb.definitions.path}")
     public void setDefinitionsPath(final String definitionsPath) {
@@ -44,6 +50,7 @@ public class DecoderDefinition {
                 documentBuilderFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
                 final DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
                 this.decoderDefDocument = documentBuilder.parse(xmlStream);
+                buildCVDefinitions();
             } else {
                 throw new DefinitionException(String.format("File Not Found %s", decoderDefFile));
             }
@@ -63,6 +70,71 @@ public class DecoderDefinition {
             xmlStream = DecoderDefinition.class.getResourceAsStream(decoderDefFile);
         }
         return xmlStream;
+    }
+
+    private void buildCVDefinitions() throws DefinitionException {
+        cvDefinitions = new TreeSet<>();
+        buildLocalCvDefinitions();
+    }
+
+    private void buildLocalCvDefinitions() throws DefinitionException {
+        final NodeList cvNodes = getCVNodes();
+        for(int index = 0; index < cvNodes.getLength(); index++) {
+            addCV((Element) cvNodes.item(index));
+        }
+    }
+
+    private void addCV(final Element cvElement) {
+        final CVDefinition cv = new CVDefinition();
+        cv.setNumber(cvElement.getAttribute("number"));
+        cv.setValues(fetchValues(cvElement.getElementsByTagName("value")));
+        cvDefinitions.add(cv);
+    }
+
+    private Collection<CVValue> fetchValues(final NodeList valueList) {
+        final Collection<CVValue> values = new TreeSet<>();
+        for(int index = 0; index < valueList.getLength(); index++) {
+            addCVValue(values, (Element) valueList.item(index));
+        }
+        return values;
+    }
+
+    private void addCVValue(final Collection<CVValue> values, final Element valueElement) {
+        final CVValue value = new CVValue();
+        value.setId(valueElement.getAttribute("id"));
+        value.setName(valueElement.getAttribute("name"));
+        value.setType(CVValue.Type.valueOf(valueElement.getAttribute("type").toUpperCase()));
+        value.setBit(valueToInteger(valueElement.getAttribute("bit")));
+        value.setLow(valueToInteger(valueElement.getAttribute("low")));
+        value.setHigh(valueToInteger(valueElement.getAttribute("high")));
+        value.setMask(valueToInteger(valueElement.getAttribute("mask")));
+        value.setOptions(fetchOptions(valueElement.getElementsByTagName("option")));
+        values.add(value);
+    }
+
+    private Integer valueToInteger(final String value) {
+        final Integer integer;
+        if (value == null || value.isEmpty()) {
+            integer = null;
+        } else {
+            integer = Integer.parseInt(value);
+        }
+        return integer;
+    }
+
+    private Collection<CVValueOption> fetchOptions(final NodeList optionsList) {
+        final Collection<CVValueOption> options = new TreeSet<>();
+        for(int index = 0; index < optionsList.getLength(); index++) {
+            addCVOption(options, (Element) optionsList.item(index));
+        }
+        return options;
+    }
+
+    private void addCVOption(final Collection<CVValueOption> options, final Element optionElement) {
+        final CVValueOption option = new CVValueOption();
+        option.setValue(Integer.parseInt(optionElement.getAttribute("value")));
+        option.setName(optionElement.getAttribute("name"));
+        options.add(option);
     }
 
     public Node getValueNode(final String valueName) throws DefinitionException {
