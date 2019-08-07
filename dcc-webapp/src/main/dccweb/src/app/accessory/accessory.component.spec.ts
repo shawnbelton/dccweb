2df2d9adb5e4711631198de4ee8454a4fb99d9cd
Shawn  Belton <shawn.belton@btinternet.com>
2019-08-07 21:52:00 +0100

    Add unit tests to Angular

--- dcc-webapp/src/main/dccweb/src/app/accessory/accessory.component.spec.ts
+++ dcc-webapp/src/main/dccweb/src/app/accessory/accessory.component.spec.ts
@@ -1,14 +1,22 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {AccessoryComponent} from './accessory.component';
+import {AccessoryDecoderService} from '../accessory-decoder.service';
+import {MacroService} from '../macro.service';
 
 describe('AccessoryComponent', () => {
   let component: AccessoryComponent;
   let fixture: ComponentFixture<AccessoryComponent>;
 
   beforeEach(async(() => {
+    const accessoryDecoderService = jasmine.createSpyObj('AccessoryDecoderService', ['getAccessoryTypes', 'saveAccessory']);
+    const macroService = jasmine.createSpyObj('MacroService', ['getMacros'])
     TestBed.configureTestingModule({
-      declarations: [ AccessoryComponent ]
+      declarations: [AccessoryComponent],
+      providers: [
+        {provide: AccessoryDecoderService, useValue: accessoryDecoderService},
+        {provide: MacroService, useValue: macroService}
+      ]
     })
     .compileComponents();
   }));
