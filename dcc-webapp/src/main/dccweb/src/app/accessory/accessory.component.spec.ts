068452710b878cd60b9cb3a8dcae7532c4e5450d
Shawn  Belton <shawn.belton@btinternet.com>
2019-08-08 20:48:54 +0100

    Add unit tests to Angular

--- dcc-webapp/src/main/dccweb/src/app/accessory/accessory.component.spec.ts
+++ dcc-webapp/src/main/dccweb/src/app/accessory/accessory.component.spec.ts
@@ -3,15 +3,33 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {AccessoryComponent} from './accessory.component';
 import {AccessoryDecoderService} from '../accessory-decoder.service';
 import {MacroService} from '../macro.service';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FormsModule} from '@angular/forms';
+import {of} from 'rxjs';
 
 describe('AccessoryComponent', () => {
   let component: AccessoryComponent;
   let fixture: ComponentFixture<AccessoryComponent>;
 
   beforeEach(async(() => {
-    const accessoryDecoderService = jasmine.createSpyObj('AccessoryDecoderService', ['getAccessoryTypes', 'saveAccessory']);
-    const macroService = jasmine.createSpyObj('MacroService', ['getMacros'])
+    const accessoryDecoderService = jasmine.createSpyObj('AccessoryDecoderService', {
+      'getAccessoryTypes': of([
+        {
+          decoderTypeId: 1,
+          decoderType: 'Point'
+        }
+      ]), 'saveAccessory': null
+    });
+    const macroService = jasmine.createSpyObj('MacroService', {
+      'getMacros': of([
+        {
+          macroId: 1,
+          name: 'Macro'
+        }
+      ])
+    });
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule, FormsModule],
       declarations: [AccessoryComponent],
       providers: [
         {provide: AccessoryDecoderService, useValue: accessoryDecoderService},
@@ -24,6 +42,19 @@ describe('AccessoryComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AccessoryComponent);
     component = fixture.componentInstance;
+    component.editAccessory = {
+      address: 123,
+      accessoryDecoderId: 1,
+      accessoryDecoderType: {
+        decoderTypeId: 1,
+        decoderType: 'Point',
+        decoderTypeCode: 'POINT',
+        decoderTypeOperations: null
+      },
+      name: 'Test Point',
+      currentValue: 1,
+      macroId: null
+    };
     fixture.detectChanges();
   });
 
